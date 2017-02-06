f9aaf061ec92d6bb934838c8bd7824c5c0c1992a
pdfarhad <pdcsedu@gmail.com>
2017-02-06 15:16:27 +0600

    autofixture added and chapter 5 completed

--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^',include('rango.urls')),
+    url(r'^', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^rango/', include('rango.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
