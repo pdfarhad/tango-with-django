615e1efd2b18818c11baea5a3a4a556acb5726da
pdfarhad <pdcsedu@gmail.com>
2017-02-08 16:31:55 +0600

    chapter 8 added

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,49 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response, render
 from .models import Category, Page
 from .forms import CategoryForm, UserForm, UserProfileForm
+from django.contrib.auth.decorators import login_required
+
+
+def user_logout(request):
+    logout(request)
+
+    return HttpResponseRedirect('/rango/')
+
+
+@login_required()
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see the text!")
+
+
+def user_login(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username= username, password=password)
+
+        if user is not None:
+            if user.is_active:
+
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse('Your Rango account is disabled')
+
+        else:
+            print("Invalid Login details:{}, {}".format(username, password))
+
+            return HttpResponse("Invalid Login details supplied")
+
+    else:
+        return render(request, 'rango/login.html', {}, context)
+
 
 @csrf_protect
 def add_category(request):
@@ -28,6 +67,7 @@ def add_category(request):
 
     # return render_to_response('rango/add_category.html', {'form': form}, context)
 
+@csrf_protect
 def register(request):
     context = RequestContext(request)
     registered = False
@@ -55,6 +95,16 @@ def register(request):
 
         else:
             print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'rango/register.html',
+                  {'user_form': user_form, 'profile_form':profile_form,
+                   'registered': registered},
+                  context)
+
 
 def index(request):
     context = RequestContext(request)
